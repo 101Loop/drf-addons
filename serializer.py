d3b991cdae55e95bce6b415bc04a931330e524ce
Himanshu Shankar <amhssingh@gmail.com>
2017-10-05 15:31:31 +0530

    Removed `SpecificSearchSerializer` Removed `where` in `SearchSerializer`

--- serializer.py
+++ serializer.py
@@ -3,14 +3,6 @@ from rest_framework import serializers
 
 class SearchSerializer(serializers.Serializer):
     listf = ('id', 'create_date', 'last_modified')
-    where = serializers.ListField(default=[])
-    order_by = serializers.ListField(child=serializers.ChoiceField(choices=listf+tuple('-'+x for x in listf)), default=['create_date'])
-    paginator = serializers.IntegerField(default=10)
-    page = serializers.IntegerField(default=1)
-
-
-class SpecificSearchSerializer(serializers.Serializer):
-    listf = ('id', 'create_date', 'last_modified')
     order_by = serializers.ListField(child=serializers.ChoiceField(choices=listf+tuple('-'+x for x in listf)), default=['create_date'])
     paginator = serializers.IntegerField(default=10)
     page = serializers.IntegerField(default=1)
