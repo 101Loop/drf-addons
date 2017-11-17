e6771141481bda8c4680a90a5a528ae4dc4ac74c
Himanshu Shankar <amhssingh@gmail.com>
2017-11-17 23:38:11 +0530

    Default sorting field updated
    Changed `default` value of `sort_by` to `id` from `create_date` as some model may not have `create_date` but `id` is compulsory and generally will provide same output.

--- serializer.py
+++ serializer.py
@@ -3,6 +3,6 @@ from rest_framework import serializers
 
 class SearchSerializer(serializers.Serializer):
     listf = ('id', 'create_date', 'last_modified')
-    order_by = serializers.ListField(child=serializers.ChoiceField(choices=listf+tuple('-'+x for x in listf)), default=['create_date'])
+    order_by = serializers.ListField(child=serializers.ChoiceField(choices=listf+tuple('-'+x for x in listf)), default=['id'])
     paginator = serializers.IntegerField(default=10)
     page = serializers.IntegerField(default=1)
