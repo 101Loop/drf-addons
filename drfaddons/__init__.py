f78b4d65394e464c59e683315e7d4661a41966cb
Himanshu Shankar <me@himanshus.com>
2018-07-28 11:55:33 +0530

    Updated 'version'

--- drfaddons/__init__.py
+++ drfaddons/__init__.py
@@ -1,6 +1,6 @@
 name = "drfaddons"
 
 __title__ = 'Django REST Framework - Add Ons'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __author__ = 'Civil Machines Technologies Private Limited'
 __license__ = 'GPLv3'
