72bac69734a0b97ec34787b548e87155e1882e03
Himanshu Shankar <me@himanshus.com>
2018-10-21 13:21:43 +0530

    Updated `generics`, `serializers` & `requirements`

--- drfaddons/generics.py
+++ drfaddons/generics.py
@@ -16,8 +16,8 @@ class OwnerGenericAPIView(GenericAPIView):
     Uses JSONParser & JSONRenderer
     """
     from .filters import IsOwnerFilterBackend
+    from .permissions import IsOwner
 
-    from rest_framework.permissions import IsAuthenticated
     from rest_framework.renderers import JSONRenderer
     from rest_framework.parsers import JSONParser
 
@@ -26,15 +26,21 @@ class OwnerGenericAPIView(GenericAPIView):
     # The filter backend classes to use for queryset filtering
     filter_backends = (IsOwnerFilterBackend, DjangoFilterBackend)
 
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsOwner, )
     renderer_classes = (JSONRenderer, )
     parser_classes = (JSONParser, )
-    
-    def check_object_permissions(self, request, obj):
-        if obj.created_by != request.user:
-            self.permission_denied(request,
-                                   message="Unsufficient permission. You are not the owner of requested object.")
-        super(OwnerGenericAPIView, self).check_object_permissions(request=request, obj=obj)
+
+
+class GenericByUserAPIView(OwnerGenericAPIView):
+    """
+    Generic view where object is retrieved via logged in user and does not requires a primary key.
+    Can be used in models having One-to-One relation with User model.
+    """
+    lookup_field = 'created_by'
+
+    def get_object(self):
+        self.kwargs[self.lookup_field] = self.request.user
+        return super(GenericByUserAPIView, self).get_object()
 
 
 # Concrete view classes that provide method handlers
@@ -149,3 +155,114 @@ class OwnerRetrieveUpdateDestroyAPIView(mixins.RetrieveModelMixin,
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+
+class RetrieveByUserAPIView(mixins.RetrieveModelMixin,
+                            GenericByUserAPIView):
+    """
+    Concrete view for retrieving a CreateUpdateModel based model instance where One-to-One relationship exists on
+    created_by with User.
+    """
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
+class UpdateByUserAPIView(OwnerUpdateModelMixin,
+                          GenericByUserAPIView):
+    """
+    Concrete view for updating a CreateUpdateModel based model instance where One-to-One relationship exists on
+    created_by with User.
+    """
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+
+class DestroyByUserAPIView(mixins.DestroyModelMixin,
+                           GenericByUserAPIView):
+    """
+    Concrete view for deleting a CreateUpdateModel based model instance where One-to-One relationship exists on
+    created_by with User.
+    """
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class RetrieveUpdateByUserAPIView(mixins.RetrieveModelMixin,
+                                  OwnerUpdateModelMixin,
+                                  GenericByUserAPIView):
+    """
+    Concrete view for retrieving, updating a CreateUpdateModel based model instance where One-to-One relationship
+    exists on created_by with User.
+    """
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+
+class RetrieveDestroyByUserAPIView(mixins.RetrieveModelMixin,
+                                   mixins.DestroyModelMixin,
+                                   GenericByUserAPIView):
+    """
+    Concrete view for retrieving or deleting a CreateUpdateModel based model instance where One-to-One relationship
+    exists on created_by with User.
+    """
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class RetrieveUpdateDestroyByUserAPIView(mixins.RetrieveModelMixin,
+                                         OwnerUpdateModelMixin,
+                                         mixins.DestroyModelMixin,
+                                         GenericByUserAPIView):
+    """
+    Concrete view for retrieving, updating or deleting a CreateUpdateModel based model instance where One-to-One
+    relationship exists on created_by with User.
+    """
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class CreateRetrieveUpdateDestroyByUserAPIView(OwnerCreateModelMixin,
+                                               mixins.RetrieveModelMixin,
+                                               OwnerUpdateModelMixin,
+                                               mixins.DestroyModelMixin,
+                                               GenericByUserAPIView):
+    """
+    Concrete view for adding, retrieving, updating or deleting a CreateUpdateModel based model instance where
+    One-to-One relationship exists on created_by with User.
+    """
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
--- drfaddons/serializers.py
+++ drfaddons/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from django.utils.text import gettext_lazy as _
+
 
 class SearchSerializer(serializers.Serializer):
     """
@@ -14,3 +16,15 @@ class SearchSerializer(serializers.Serializer):
     order_by = serializers.ListField(child=serializers.ChoiceField(choices=listf+tuple('-'+x for x in listf)), default=['id'])
     paginator = serializers.IntegerField(default=10)
     page = serializers.IntegerField(default=1)
+
+
+class ByOwnerSerializer(serializers.ModelSerializer):
+
+    def validate(self, attrs):
+        model = self.Meta.model
+
+        if model.objects.filter(created_by=self.context['request'].user).count() > 0:
+            raise serializers.ValidationError(detail=_('Logged in user alread has %s object. Can not create another'
+                                                       ' object.' % (model._meta.verbose_name.title())))
+
+        return attrs
